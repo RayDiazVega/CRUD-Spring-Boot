e1576262fc62c00073fc0e7dae1be2c76db3c0f1
Rython <r.diazvega@zabud.com.co>
2020-01-10 16:24:35 -0500

    helth check enable

--- src/main/java/com/example/demo/CrudApplication.java
+++ src/main/java/com/example/demo/CrudApplication.java
@@ -4,8 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAdminServer
+@EnableSwagger2
 public class CrudApplication {
 
 	public static void main(String[] args) {
