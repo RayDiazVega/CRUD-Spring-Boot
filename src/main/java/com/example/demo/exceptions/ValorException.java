1ca84ca6f663512f1e558128084c0354d8336e85
Rython <r.diazvega@zabud.com.co>
2020-01-13 15:26:35 -0500

    space

--- src/main/java/com/example/demo/exceptions/ValorException.java
+++ src/main/java/com/example/demo/exceptions/ValorException.java
@@ -2,7 +2,13 @@ package com.example.demo.exceptions;
 
 public class ValorException extends RuntimeException {
 	
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public ValorException() {
 		super("El valor debe ser mayor o igual a 1");
 	}
+	
 }
