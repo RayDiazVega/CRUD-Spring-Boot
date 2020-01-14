f8bf00ff9cd10286dea39fe3e41052515a14bee1
Rython <r.diazvega@zabud.com.co>
2020-01-14 10:47:44 -0500

    swagger fixed

--- src/main/java/com/example/demo/exceptions/RegistroNoEncontradoException.java
+++ src/main/java/com/example/demo/exceptions/RegistroNoEncontradoException.java
@@ -2,11 +2,6 @@ package com.example.demo.exceptions;
 
 public class RegistroNoEncontradoException extends RuntimeException {
 	
-	/**
-	 *
-	 */
-	private static final long serialVersionUID = 1L;
-
 	public RegistroNoEncontradoException() {
 		super("Elemento no encontrado");
 	}
--- src/main/java/com/example/demo/exceptions/ValorException.java
+++ src/main/java/com/example/demo/exceptions/ValorException.java
@@ -2,13 +2,7 @@ package com.example.demo.exceptions;
 
 public class ValorException extends RuntimeException {
 	
-	/**
-	 *
-	 */
-	private static final long serialVersionUID = 1L;
-
 	public ValorException() {
 		super("El valor debe ser mayor o igual a 1");
 	}
-	
 }
